36e28629fa7addc48c1b1ea607dfe327ea26e315
Berry <berry@berrydejager.com>
2022-10-07 13:13:03 +0200

    Update compile.sh

--- local-development/compile.sh
+++ local-development/compile.sh
@@ -1,7 +1,12 @@
 # Make sure that JAVA is installed
 # sudo apt --yes install openjdk-11-jre
 
-java -jar KickAss.jar -showmem ../sources/main-left.asm
-java -jar KickAss.jar -showmem ../sources/main-right.asm
-perl 1541u2.pl 1541u2-00 -c main-left.prg
-perl 1541u2.pl 1541u2-01 -c main-right.prg
+echo "compiling -left"
+time java -jar KickAss.jar -showmem ../sources/main-left.asm
+echo "compiling -right"
+time java -jar KickAss.jar -showmem ../sources/main-right.asm
+
+echo "deploying -left"
+time perl 1541u2.pl 1541u2-00 -c run:../sources/main-left.prg
+echo "deploying -right"
+time perl 1541u2.pl 1541u2-01 -c run:../sources/main-right.prg
