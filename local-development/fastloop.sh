1776254124760c6d227897a0f422979630002e3e
Berry <berry@berrydejager.com>
2022-10-09 00:12:21 +0200

    Update fastloop.sh

--- local-development/fastloop.sh
+++ local-development/fastloop.sh
@@ -1,3 +1,15 @@
 clear
 java -jar KickAss.jar -showmem ../sources/main-left.asm
 perl 1541u2.pl u64 -c run:../sources/main-left.prg
+
+read -rsp $'Press any key to continue...\n' -n1 key
+
+# ./fastloop.sh
+
+java -jar KickAss.jar -showmem ../sources/main-right.asm
+perl 1541u2.pl u64 -c run:../sources/main-right.prg
+
+read -rsp $'Press any key to continue...\n' -n1 key
+
+./fastloop.sh
+
