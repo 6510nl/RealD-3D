77cf4c83ae96a1aaaf2af7455370d09fda8dc91e
Berry <berry@berrydejager.com>
2022-10-11 15:34:58 +0200

    Update u64.sh

--- local-development/u64.sh
+++ local-development/u64.sh
@@ -4,7 +4,7 @@ perl 1541u2.pl u64 -c run:../sources/main-left.prg
 
 read -rsp $'Press any key to continue...\n' -n1 key
 
-# ./fastloop.sh
+./$0
 
 java -jar KickAss.jar -showmem ../sources/main-right.asm
 perl 1541u2.pl u64 -c run:../sources/main-right.prg
